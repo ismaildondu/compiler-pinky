d12518959d5a7a7f77787540455f228020275717
ismail Döndü <ismaildondu7@gmail.com>
2025-11-22 23:04:05 +0300

    Add support for String type in interpreter; enhance arithmetic operations with type checks and error handling

--- interpreter.py
+++ interpreter.py
@@ -15,6 +15,8 @@ class Interpreter:
             return (TYPE_NUMBER, float(astNode.value))
         if isinstance(astNode, FloatModel):
             return (TYPE_NUMBER, float(astNode.value))
+        if isinstance(astNode, StringModel):
+            return (TYPE_STRING, str(astNode.value))
         if isinstance(astNode, GroupingModel):
             return self.interpret(astNode.expression)
         if isinstance(astNode, BinaryOperationModel):
@@ -28,11 +30,22 @@ class Interpreter:
                 else:
                     runtime_error(f"Unsupported operand types for +: '{leftType}' and '{rightType}'", astNode.line)
             elif astNode.operator.token_type == TOK_MINUS:
-                return left - right
+                if rightType == TYPE_NUMBER and leftType == TYPE_NUMBER:
+                    return (TYPE_NUMBER, leftVal - rightVal)
+                else:
+                    runtime_error(f"Unsupported operand types for -: '{leftType}' and '{rightType}'", astNode.line)
             elif astNode.operator.token_type == TOK_STAR:
-                return left * right
+                if rightType == TYPE_NUMBER and leftType == TYPE_NUMBER:
+                    return (TYPE_NUMBER, leftVal * rightVal)
+                else:
+                    runtime_error(f"Unsupported operand types for *: '{leftType}' and '{rightType}'", astNode.line)
             elif astNode.operator.token_type == TOK_SLASH:
-                return left / right
+                if rightType == TYPE_NUMBER and leftType == TYPE_NUMBER:
+                    if rightVal == 0:
+                        runtime_error("Division by zero", astNode.line)
+                    return (TYPE_NUMBER, leftVal / rightVal)
+                else:
+                    runtime_error(f"Unsupported operand types for /: '{leftType}' and '{rightType}'", astNode.line)
         if isinstance(astNode, UnaryOperationModel):
             operand = self.interpret(astNode.operand)
             if astNode.operator.token_type == TOK_MINUS:
